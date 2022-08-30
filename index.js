3d53bf1892cf26ceb572f60a826cbcb16483b7f1
Alex Knott <alextknott@gmail.com>
2022-08-30 19:50:36 +0100

    Handle routes and send a response based on the url

--- index.js
+++ index.js
@@ -4,7 +4,19 @@ const app = express();
 const port = 3000;
 
 app.get("/", (req, res) => {
-  res.send("Hello World!");
+  res.send("Index page");
+});
+
+app.get("/about", (req, res) => {
+  res.send("About page");
+});
+
+app.get("/contact-me", (req, res) => {
+  res.send("Contact page");
+});
+
+app.use((req, res, next) => {
+  res.status(404).send("Sorry can't find that!");
 });
 
 app.listen(port, () => {
